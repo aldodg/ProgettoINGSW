fce4f95547961eeaa3043b95e89c74c0f09e3725
Aldo Di Giovanni <50913753+aldodg@users.noreply.github.com>
2022-01-06 20:47:59 +0100

    API OK
    manca login

---
+++ Backend/PHP/localhost/getFriendshipRequest_idUser.php
@@ -0,0 +1,26 @@
+<?php
+	$conn = mysqli_connect("localhost","root","","cinematesdb");
+
+	/* id1 e' l'id dell'utente che invia la richiesta di amicizia a quello
+	che ha id2; la query devo quindi farla su id2 dandogli l'id di colui che sta
+	vedendo quali richieste di amicizia gli sono arrivate*/
+
+	$id = isset($_GET['id']) ? mysqli_real_escape_string($conn, $_GET['id']) :  "";
+	$sql = "SELECT * FROM cinematesdb.friendship WHERE id2='{$id}' AND state='pending';";
+	$get_data_query = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+		if(mysqli_num_rows($get_data_query)!=0){
+		$result = array();
+
+		while($r = mysqli_fetch_array($get_data_query)){
+			extract($r);
+			$result[] = array("id1" => $id1);
+		}
+		$json = array("status" => 1, "info" => $result);
+	}
+	else{
+		$json = array("status" => 0, "error" => "not found!");
+	}
+@mysqli_close($conn);
+// Set Content-type to JSON
+header('Content-type: application/json');
+echo json_encode($json);
---
+++ Backend/PHP/localhost/postNewFriendshipRequest.php
@@ -0,0 +1,27 @@
+<?php
+$conn = mysqli_connect("localhost","root","","cinematesdb");
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+	// Get data from the REST client
+	$id1 = isset($_POST['id1']) ? mysqli_real_escape_string($conn, $_POST['id1']) : "";
+	$id2 = isset($_POST['id2']) ? mysqli_real_escape_string($conn, $_POST['id2']) : "";
+	$state = isset($_POST['state']) ? mysqli_real_escape_string($conn, $_POST['state']) : "";
+	// Insert data into database
+	//RICORDA CHE PENDING DEVE ANDARE FRA VIRGOLETTE DURANTE LE QUERY DA DBEAVER
+	//SU POSTMAN INVECE NON CI VANNO LE VIRGOLETTE
+	$sql = "INSERT INTO cinematesdb.friendship (id1, id2, state) VALUES ('$id1', '$id2', '$state');";
+	$post_data_query = mysqli_query($conn, $sql);
+	if($post_data_query){
+		$json = array("status" => 1, "Success" => "has been added successfully!");
+	}
+	else{
+		$json = array("status" => 0, "Error" => "Error adding! Please try again!");
+	}
+}
+else{
+	$json = array("status" => 0, "Info" => "Request method not accepted!");
+}
+@mysqli_close($conn);
+// Set Content-type to JSON
+header('Content-type: application/json');
+echo json_encode($json);
