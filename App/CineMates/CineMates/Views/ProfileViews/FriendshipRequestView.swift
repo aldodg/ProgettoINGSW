4885b28c50859e8a346843a13a2e8b65fe5e808d
Aldo Di Giovanni <50913753+aldodg@users.noreply.github.com>
2022-01-12 06:25:52 +0100

    richieste amicizia in lavorazione MANCANO BOTTONI

--- App/CineMates/CineMates/Views/ProfileViews/FriendshipRequestView.swift
+++ App/CineMates/CineMates/Views/ProfileViews/FriendshipRequestView.swift
@@ -8,30 +8,55 @@
 
 import SwiftUI
 
+
 struct FriendshipRequestView: View {
-    @State var infos = [Informations]()
-    let loader = Loader()
+    @State var friendshipRequests = [FriendshipRequest]()
     
     var body: some View {
-        List{
-            ForEach(self.infos) { section in
-                Text(section.idMovie)
-                }.onAppear(){
-                    print("ID MOVIE \(self.infos[0].idMovie)--\(self.infos[1].idMovie)")
-                }
-        }.navigationTitle("Link Request").listStyle(.grouped).onAppear(){
-            let urlString = "http://ec2-3-250-182-218.eu-west-1.compute.amazonaws.com/getUserComplete_idUser.php/info?id=1"
-                loader.loadData(from: urlString){ result in
-                
-                //safe unwrap list, at this stage, it should not be nil
-                guard let result = result else {
-                    print("ContentView: List is nil!")
-                    return
-                }
-                self.infos = result
-                print("ContentView: Successfully populated infoItems with \(infos.count) item(s)!")
+        ForEach(self.friendshipRequests) { request in
+            NavigationLink(destination: UserCompleteView(request: request)){
+                //qua mettere username e bottoni per accettare o rifiutare
+                Text(request.id)
+            }
+            
         }
+        Text("")
+            .padding()
+            .onAppear() {
+                ApiFriendshipRequest().loadDataRequest { (list) in
+                    self.friendshipRequests = list.friendshipRequests
+                }
+            }.navigationTitle("Friendship Requests")
     }
 }
 
-}
+
+
+
+//struct FriendshipRequestView: View {
+//    @State var infos = [Informations]()
+//    let loader = Loader()
+//
+//    var body: some View {
+//        List{
+//            ForEach(self.infos) { section in
+//                Text(section.idMovie)
+//                }.onAppear(){
+//                    print("ID MOVIE \(self.infos[0].idMovie)--\(self.infos[1].idMovie)")
+//                }
+//        }.navigationTitle("Link Request").listStyle(.grouped).onAppear(){
+//            let urlString = "http://ec2-3-250-182-218.eu-west-1.compute.amazonaws.com/getUserComplete_idUser.php/info?id=1"
+//                loader.loadData(from: urlString){ result in
+//
+//                //safe unwrap list, at this stage, it should not be nil
+//                guard let result = result else {
+//                    print("ContentView: List is nil!")
+//                    return
+//                }
+//                self.infos = result
+//                print("ContentView: Successfully populated infoItems with \(infos.count) item(s)!")
+//        }
+//    }
+//}
+//
+//}
---
+++ App/CineMates/CineMates/Views/ProfileViews/UserCompleteView.swift
@@ -0,0 +1,22 @@
+//
+//  UserCompleteView.swift
+//  Cinemates
+//
+//  Created by Aldo Di Giovanni on 12/01/22.
+//  Copyright © 2022 Aldo Di Giovanni. All rights reserved.
+//
+
+import SwiftUI
+
+struct UserCompleteView: View {
+    let request: FriendshipRequest
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+//struct UserCompleteView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        UserCompleteView()
+//    }
+//}
